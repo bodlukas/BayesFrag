9015526d43844a8b4c18d5ae050be176a5a4e126
bodlukas <bodlukas@ethz.ch>
2023-10-20 16:01:12 +0200

    update inference

--- modules/inference.py
+++ modules/inference.py
@@ -6,17 +6,57 @@ from jax.scipy.stats import norm
 import numpyro
 import numpyro.distributions as dist
 
-def get_pmf_ds_logIM(logIM, etas, beta, bc, n_ds):
+# def get_pmf_ds_logIM(logIM, etas, beta, bc, n_ds):
+#     '''
+#     Evaluate fragility functions -> Computes damage state probabilities 
+#     conditional on: logIM, parameters (etas, beta) and building classes
+
+#     args:
+#         logIM (ArrayLike): logarithmic intensity measure in unit g
+#             dimension: (n_sites,)
+
+#         etas (ArrayLike): threshold parameters of fragility functions 
+#             dimension: (n_bc, n_ds-1)
+
+#         beta (ArrayLike): dispersion parameters of fragility functions
+#             dimension: (n_bc,)
+
+#         bc (ArrayLike): Observed building classes (encoded with integers)
+#             dimension: (n_sites,)
+
+#         n_ds (int): Number of possible damage states
+
+#     returns:
+#         probs (ArrayLike): damage state probabilities
+#             dimension: (n_sites, n_ds)
+#     '''
+#     beta_inv = 1/beta
+#     # Evaluate fragility functions at logIM
+#     # Probability of reaching or exceeding a certain DS
+#     ccdfs = []
+#     ccdfs.append(jnp.ones_like(logIM))
+#     for ds in jnp.arange(n_ds-1):
+#       ccdfs.append(norm.cdf(logIM * beta_inv[bc] - etas[bc, ds]))
+#     ccdfs.append(jnp.zeros_like(logIM))
+#     # Compute probability of being in a certain DS
+#     probs = jnp.diff(1-jnp.stack(ccdfs), axis=0)
+#     # Add some jitter for numerical stability
+#     return probs + 1e-6
+
+def get_pmf_ds_logIM(logIM, eta1, deltas, beta, bc, n_ds):
     '''
     Evaluate fragility functions -> Computes damage state probabilities 
-    conditional on: logIM, parameters (etas, beta) and building classes
+    conditional on: logIM, parameters (eta1, deltas, beta) and building classes
 
     args:
         logIM (ArrayLike): logarithmic intensity measure in unit g
             dimension: (n_sites,)
 
-        etas (ArrayLike): threshold parameters of fragility functions 
-            dimension: (n_bc, n_ds-1)
+        eta1 (ArrayLike): first threshold parameter of fragility functions 
+            dimension: (n_bc, 1)            
+
+        deltas (ArrayLike): difference between remaining threshold parameters 
+            dimension: (n_bc, n_ds-2)
 
         beta (ArrayLike): dispersion parameters of fragility functions
             dimension: (n_bc,)
@@ -31,6 +71,10 @@ def get_pmf_ds_logIM(logIM, etas, beta, bc, n_ds):
             dimension: (n_sites, n_ds)
     '''
     beta_inv = 1/beta
+    # Transform deltas to threshold parameters etas
+    etas = jnp.append(eta1, 
+                      eta1 + jnp.cumsum(deltas, axis=1), 
+                      axis=1)    
     # Evaluate fragility functions at logIM
     # Probability of reaching or exceeding a certain DS
     ccdfs = []
@@ -42,6 +86,7 @@ def get_pmf_ds_logIM(logIM, etas, beta, bc, n_ds):
     probs = jnp.diff(1-jnp.stack(ccdfs), axis=0)
     # Add some jitter for numerical stability
     return probs + 1e-6
+
 
 def Model_MCMC(mu, L, ds, bc, n_ds, n_bc):
     '''
@@ -66,7 +111,7 @@ def Model_MCMC(mu, L, ds, bc, n_ds, n_bc):
 
     # Prior sampling distributions for fragility function parameters
     eta1 = numpyro.sample('eta1',
-                          dist.Normal(jnp.ones((n_bc, 1)) * np.log(0.2),
+                          dist.Normal(jnp.ones((n_bc, 1)) * jnp.log(0.2),
                                       jnp.ones((n_bc, 1)) * 1.5) )
 
     deltas = numpyro.sample('deltas',
@@ -77,9 +122,9 @@ def Model_MCMC(mu, L, ds, bc, n_ds, n_bc):
                           dist.InverseGamma(concentration = jnp.ones(n_bc) * 2.5,
                                                     rate = jnp.ones(n_bc) * 1.0 ) )
     
-    # Transform deltas to threshold parameters etas
-    etas = jnp.append(eta1, eta1 +
-                           jnp.cumsum(deltas, axis=1), axis=1)
+    # # Transform deltas to threshold parameters etas
+    # etas = jnp.append(eta1, eta1 +
+    #                        jnp.cumsum(deltas, axis=1), axis=1)
 
     # Prior sampling distribution of z
     z = numpyro.sample('z', dist.Normal(jnp.zeros(N), 1) )
@@ -88,7 +133,7 @@ def Model_MCMC(mu, L, ds, bc, n_ds, n_bc):
     logIM = mu + jnp.matmul(L, z)
 
     # Compute damage state probabilities
-    p = get_pmf_ds_logIM(logIM, etas, beta, bc, n_ds).T
+    p = get_pmf_ds_logIM(logIM, eta1, deltas, beta, bc, n_ds).T
 
     # Likelihood
     numpyro.sample('obs', dist.Categorical(probs = p), obs = ds)
